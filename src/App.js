c5c0d7d78c3dc3024090c3947d19b7533d3dc5d6
Francesco Mirabella <mirstuk@users.noreply.github.com>
2020-12-04 09:09:40 +0100

    changed map style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import * as parkDate from "./data/skateboard-parks.json";
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import * as parkDate from './data/skateboard-parks.json';
 
 export default function App() {
   const [viewport, setViewport] = useState({
     latitude: 45.4211,
     longitude: -75.6903,
-    width: "100vw",
-    height: "100vh",
-    zoom: 10
+    width: '100vw',
+    height: '100vh',
+    zoom: 10,
   });
   const [selectedPark, setSelectedPark] = useState(null);
 
   useEffect(() => {
     const listener = e => {
-      if (e.key === "Escape") {
+      if (e.key === 'Escape') {
         setSelectedPark(null);
       }
     };
-    window.addEventListener("keydown", listener);
+    window.addEventListener('keydown', listener);
 
     return () => {
-      window.removeEventListener("keydown", listener);
+      window.removeEventListener('keydown', listener);
     };
   }, []);
 
@@ -30,24 +30,21 @@ export default function App() {
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
         onViewportChange={viewport => {
           setViewport(viewport);
-        }}
-      >
+        }}>
         {parkDate.features.map(park => (
           <Marker
             key={park.properties.PARK_ID}
             latitude={park.geometry.coordinates[1]}
-            longitude={park.geometry.coordinates[0]}
-          >
+            longitude={park.geometry.coordinates[0]}>
             <button
               className="marker-btn"
               onClick={e => {
                 e.preventDefault();
                 setSelectedPark(park);
-              }}
-            >
+              }}>
               <img src="/skateboarding.svg" alt="Skate Park Icon" />
             </button>
           </Marker>
@@ -59,8 +56,7 @@ export default function App() {
             longitude={selectedPark.geometry.coordinates[0]}
             onClose={() => {
               setSelectedPark(null);
-            }}
-          >
+            }}>
             <div>
               <h2>{selectedPark.properties.NAME}</h2>
               <p>{selectedPark.properties.DESCRIPTIO}</p>
